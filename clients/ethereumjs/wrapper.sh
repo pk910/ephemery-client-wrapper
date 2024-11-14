0e25d66c49812d0a735d715418c433d002dc12ab
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-11-14 15:06:45 -0500

    add ethjs

---
+++ clients/ethereumjs/wrapper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+client_datadir="~/.ethereum"
+
+client_args=("$@")
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --datadir=*)
+        client_datadir="${1#*=}"
+        ;;
+    --datadir)
+        client_datadir="${2}"
+        ;;
+    esac
+    shift
+done
+
+source /wrapper/wrapper.lib.sh
+
+start_client() {
+    source $testnet_dir/nodevars_env.txt
+
+    ephemery_args=""
+    if [ -z "$(echo "${client_args[@]}" | grep "bootnodes")" ]; then
+        ephemery_args="$ephemery_args --bootnodes=$BOOTNODE_ENODE_LIST"
+    fi
+
+    echo "args: ${client_args[@]} $ephemery_args"
+    node /usr/app/packages/client/dist/esm/bin/cli.js  --datadir=$client_datadir --gethGenesis=$testnet_dir/genesis.json --rpcEngine --rpcEngineAuth=false "${client_args[@]}" $ephemery_args
+}
+
+reset_client() {
+    if [ -d $client_datadir ]; then
+        echo "[EphemeryWrapper] clearing ethjs data"
+    fi
+
+    node /usr/app/packages/client/dist/esm/bin/cli.js --datadir=$client_datadir --gethGenesis=$testnet_dir/genesis.json --rpcEngine --rpcEngineAuth=false
+}
+
+ephemery_wrapper "geth" "$client_datadir" "reset_client" "start_client"
